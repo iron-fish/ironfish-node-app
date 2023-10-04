2d2129628f33c488be383530e495eabe0fc6f169
Daniel Cortes <danielgcortes@gmail.com>
2023-10-04 14:38:55 -0600

    Testing commit hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
   rules: {
     "no-else-return": "error",
     "import/no-unresolved": "error",
-    // 'import/no-named-as-default-member': 'off',
     "import/order": [
       "error",
       {
@@ -33,6 +32,17 @@ module.exports = {
         },
       },
     ],
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: ["./tsconfig.json", "./renderer/tsconfig.json"],
+      },
+    },
   },
   ignorePatterns: ["node_modules/*", "app/*", "dist/*", "renderer/.next/*"],
 };
