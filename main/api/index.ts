a73019da731efc9845661e37917a562922245e7f
danield9tqh <danield9tqh@gmail.com>
2023-10-05 18:52:03 -0400

    Add getBalances endpoint

--- main/api/index.ts
+++ main/api/index.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from "events";
 
+import { GetBalancesResponse, RpcAsset } from "@ironfish/sdk";
 import { initTRPC } from "@trpc/server";
 import { observable } from "@trpc/server/observable";
 import { dialog } from "electron";
@@ -8,6 +9,8 @@ import { z } from "zod";
 
 import { ironfish } from "./ironfish";
 import { mainWindow } from "../main-window";
+
+type Unpacked<T> = T extends (infer U)[] ? U : T;
 
 const ee = new EventEmitter();
 const t = initTRPC.create({ isServer: true });
@@ -41,6 +44,37 @@ export const router = t.router({
       };
     });
   }),
+  getBalances: t.procedure
+    .input(z.object({ account: z.string(), confirmations: z.number() }))
+    .query(async (opts) => {
+      const { input } = opts;
+
+      const rcpClient = await ironfish.getRpcClient();
+
+      const balancesStream = await rcpClient.wallet.getAccountBalances(input);
+      const accountsResponse = await balancesStream.waitForEnd();
+
+      type BalanceResponse = Unpacked<GetBalancesResponse["balances"]> & {
+        asset: RpcAsset;
+      };
+      const balances: BalanceResponse[] = [];
+
+      for (const balance of accountsResponse.content.balances) {
+        const assetStream = await rcpClient.chain.getAsset({
+          id: balance.assetId,
+        });
+        const assetResponse = await assetStream.waitForEnd();
+        balances.push({
+          ...balance,
+          asset: assetResponse.content,
+        });
+      }
+
+      return {
+        ...accountsResponse.content,
+        balances,
+      };
+    }),
   getAccounts: t.procedure.query(async () => {
     const rcpClient = await ironfish.getRpcClient();
 
