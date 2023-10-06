8102776089e9423d205dc1c9477812eb1f90e7d0
Daniel Cortes <danielgcortes@gmail.com>
2023-10-06 11:51:40 -0600

    Search input

--- renderer/components/UserAccountsList/UserAccountsList.tsx
+++ renderer/components/UserAccountsList/UserAccountsList.tsx
@@ -1,4 +1,12 @@
-import { VStack } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import {
+  VStack,
+  HStack,
+  InputGroup,
+  InputLeftElement,
+  Input,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 import { trpcReact } from "@/providers/TRPCProvider";
 import { gradientOptions } from "@/ui/ShadowCard/ShadowCard";
@@ -6,23 +14,40 @@ import { gradientOptions } from "@/ui/ShadowCard/ShadowCard";
 import { AccountRow } from "../AccountRow/AccountRow";
 
 export function UserAccountsList() {
+  const [searchInput, setSearchInput] = useState("");
   const { data } = trpcReact.getAccounts.useQuery();
-
-  console.log(data);
 
   return (
     <VStack>
-      {data?.map((account, i) => {
-        return (
-          <AccountRow
-            key={i}
-            color={gradientOptions[i % gradientOptions.length]}
-            name={account.name}
-            balance={parseInt(account.balances[0].confirmed) / 100000000}
-            address={account.address}
+      <HStack w="100%" mb={4}>
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <SearchIcon color="black" _dark={{ color: "white" }} />
+          </InputLeftElement>
+          <Input
+            type="text"
+            placeholder="Search"
+            borderColor="black"
+            onChange={(e) => setSearchInput(e.target.value)}
+            _dark={{ borderColor: "white" }}
           />
-        );
-      })}
+        </InputGroup>
+      </HStack>
+      {data
+        ?.filter((item) => {
+          return item.name.toLowerCase().includes(searchInput.toLowerCase());
+        })
+        .map((account, i) => {
+          return (
+            <AccountRow
+              key={account.address}
+              color={gradientOptions[i % gradientOptions.length]}
+              name={account.name}
+              balance={parseInt(account.balances[0].confirmed) / 100000000}
+              address={account.address}
+            />
+          );
+        })}
     </VStack>
   );
 }
