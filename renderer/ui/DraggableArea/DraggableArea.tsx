cfab90b09d3febaa7bc62d050583c7aff04a7b7b
Derek Guenther <derek@iflabs.network>
2023-11-30 17:32:19 -0500

    Cleanup

--- renderer/ui/DraggableArea/DraggableArea.tsx
+++ renderer/ui/DraggableArea/DraggableArea.tsx
@@ -1,14 +1,11 @@
-import { Box, useColorModeValue } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 export function DraggableArea() {
-  const bg = useColorModeValue("#ffffff", "#111111");
-
   return (
     <Box
-      bg={bg}
       position="fixed"
-      top="0"
-      left="0"
+      top={0}
+      left={0}
       w="100%"
       h="env(titlebar-area-height, 30px)"
       zIndex={9999}
