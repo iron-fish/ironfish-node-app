3b8765c714b9c036652c5c026e3e820b2e758984
Derek Guenther <dguenther9@gmail.com>
2023-12-04 17:32:49 -0500

    Disable Windows code signing on local builds

--- build/sign-windows.js
+++ build/sign-windows.js
@@ -2,6 +2,17 @@ const { exec } = require("child_process");
 
 exports.default = async function (configuration) {
   return new Promise((resolve, reject) => {
+    if (
+      process.env.CI === undefined ||
+      process.env.CI.toLowerCase().trim() === "false"
+    ) {
+      console.warn(
+        "CI environment variable is unset or false, skipping code signing",
+      );
+      resolve();
+      return;
+    }
+
     const cmd = `AzureSignTool sign -kvu "${process.env.AZURE_KEY_VAULT_URI}" -kvi "${process.env.AZURE_CLIENT_ID}" -kvt "${process.env.AZURE_TENANT_ID}" -kvs "${process.env.AZURE_CLIENT_SECRET}" -kvc "${process.env.AZURE_CERT_NAME}" -tr http://timestamp.digicert.com -v "${configuration.path}"`;
 
     exec(cmd, (error, stdout, stderr) => {
