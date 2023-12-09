85e019577e2633c1a840f7f97ce57dc6ee18c44b
Daniel Cortes <danielgcortes@gmail.com>
2023-12-09 00:05:47 -0700

    Shift components aronud

--- renderer/layouts/OnboardingLayout.tsx
+++ renderer/layouts/OnboardingLayout.tsx
@@ -9,8 +9,8 @@ import { WithDraggableArea } from "./WithDraggableArea";
 export function OnboardingLayout({ children }: { children: ReactNode }) {
   return (
     <WithDraggableArea>
-      <LightMode>
-        <Box height="100%" width="100%" overflow="hidden" position="relative">
+      <Box height="100%" width="100%" overflow="hidden" position="relative">
+        <LightMode>
           <Box
             height="150vh"
             width="100%"
@@ -38,8 +38,8 @@ export function OnboardingLayout({ children }: { children: ReactNode }) {
           >
             {children}
           </Box>
-        </Box>
-      </LightMode>
+        </LightMode>
+      </Box>
     </WithDraggableArea>
   );
 }
