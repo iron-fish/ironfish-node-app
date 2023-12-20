02b05e61c74faa870f5df9a6daf24ce9ed3b957f
Derek Guenther <derek@iflabs.network>
2023-12-20 17:29:43 -0500

    Update icons according to Figma

--- renderer/components/NoteRow/icons/ExpiredIcon.tsx
+++ renderer/components/NoteRow/icons/ExpiredIcon.tsx
@@ -1,25 +1,23 @@
 import { chakra, useColorModeValue } from "@chakra-ui/react";
 
-import { COLORS } from "@/ui/colors";
-
 export function ExpiredIcon() {
-  const fill = useColorModeValue(
-    COLORS.GRAY_MEDIUM,
-    COLORS.DARK_MODE.GRAY_LIGHT,
-  );
+  const fill = useColorModeValue("#FF3D00", "#C84C1B");
 
   return (
     <chakra.svg
       width={26}
       height={26}
       fill="none"
-      color={COLORS.GRAY_LIGHT}
+      color="#FFE5DD"
       _dark={{
-        color: COLORS.DARK_MODE.GRAY_MEDIUM,
+        color: "#453328",
       }}
     >
       <circle cx={13} cy={13} r={13} fill="currentColor" />
-      <path d="M18 8L8 18M8 8L18 18" stroke={fill} strokeWidth="1.5" />
+      <path
+        d="M9.8 17L9 16.2L12.2 13L9 9.8L9.8 9L13 12.2L16.2 9L17 9.8L13.8 13L17 16.2L16.2 17L13 13.8L9.8 17Z"
+        fill={fill}
+      />
     </chakra.svg>
   );
 }
