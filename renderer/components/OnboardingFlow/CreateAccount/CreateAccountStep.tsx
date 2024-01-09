895b23f3171d6f66ad6fb138d9759044d56d67d5
Daniel Cortes <danielgcortes@gmail.com>
2024-01-09 15:50:33 -0700

    Translate skipped pages

--- renderer/components/OnboardingFlow/CreateAccount/CreateAccountStep.tsx
+++ renderer/components/OnboardingFlow/CreateAccount/CreateAccountStep.tsx
@@ -1,5 +1,6 @@
 import { Box, Checkbox, Heading, Text } from "@chakra-ui/react";
 import { useState } from "react";
+import { defineMessages, useIntl } from "react-intl";
 
 import { BackButton } from "@/components/BackButton/BackButton";
 import { COLORS } from "@/ui/colors";
@@ -9,6 +10,38 @@ import {
 } from "@/ui/Forms/MnemonicPhrase/MnemonicPhrase";
 import { TextInput } from "@/ui/Forms/TextInput/TextInput";
 import { PillButton } from "@/ui/PillButton/PillButton";
+
+const messages = defineMessages({
+  createAccount: {
+    defaultMessage: "Create Account",
+  },
+  internalAccountName: {
+    defaultMessage: "Internal Account Name",
+  },
+  accountNameLabel: {
+    defaultMessage: "Account Name",
+  },
+  accountNameError: {
+    defaultMessage: "Please enter a name for this account",
+  },
+  recoveryPhrase: {
+    defaultMessage: "Recovery Phrase",
+  },
+  recoveryPhraseDescription: {
+    defaultMessage:
+      "Please keep this phrase stored somewhere safe. We will ask you to re-enter this.",
+  },
+  savedRecoveryPhrase: {
+    defaultMessage: "I saved my recovery phrase",
+  },
+  nextButton: {
+    defaultMessage: "Next",
+  },
+  accountNameDescription: {
+    defaultMessage:
+      "This name is how we will refer to your account internally. It is not visible to anyone else.",
+  },
+});
 
 export function CreateAccountStep({
   accountName,
@@ -23,36 +56,35 @@ export function CreateAccountStep({
 }) {
   const [isSavedChecked, setIsSavedChecked] = useState(false);
   const hasValidName = accountName.length > 0;
+  const { formatMessage } = useIntl();
 
   return (
     <Box>
       <BackButton />
       <Heading mt={4} mb={8}>
-        Create Account
+        {formatMessage(messages.createAccount)}
       </Heading>
       <Text fontSize="2xl" mb={1}>
-        Internal Account Name
+        {formatMessage(messages.internalAccountName)}
       </Text>
       <Text color={COLORS.GRAY_MEDIUM} mb={4}>
-        This name is how we will refer to your account internally. It is not
-        visible to anyone else.
+        {formatMessage(messages.accountNameDescription)}
       </Text>
       <TextInput
-        label="Account Name"
+        label={formatMessage(messages.accountNameLabel)}
         value={accountName}
         error={
-          hasValidName ? undefined : "Please enter a name for this account"
+          hasValidName ? undefined : formatMessage(messages.accountNameError)
         }
         onChange={(e) => {
           onNameChange(e.target.value);
         }}
       />
       <Text fontSize="2xl" mt={8} mb={1}>
-        Recovery Phrase
+        {formatMessage(messages.recoveryPhrase)}
       </Text>
       <Text color={COLORS.GRAY_MEDIUM} mb={4}>
-        Please keep this phrase stored somewhere safe. We will ask you to
-        re-enter this.
+        {formatMessage(messages.recoveryPhraseDescription)}
       </Text>
       <MnemonicPhrase readOnly phrase={splitMnemonicPhrase(mnemonicPhrase)} />
       <Checkbox
@@ -63,13 +95,13 @@ export function CreateAccountStep({
           setIsSavedChecked(e.target.checked);
         }}
       >
-        I saved my recovery phrase
+        {formatMessage(messages.savedRecoveryPhrase)}
       </Checkbox>
       <PillButton
         isDisabled={!isSavedChecked || !hasValidName}
         onClick={onNextStep}
       >
-        Next
+        {formatMessage(messages.nextButton)}
       </PillButton>
     </Box>
   );
