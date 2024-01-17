ca336eef3dda8f84c6c4a1b5f80709bf1cd9ad97
Derek Guenther <dguenther9@gmail.com>
2024-01-17 12:02:03 -0500

    Add DeleteAccountModal to replace default-styled alert dialog

--- renderer/components/AccountSettings/AccountSettings.tsx
+++ renderer/components/AccountSettings/AccountSettings.tsx
@@ -1,19 +1,9 @@
-import {
-  HStack,
-  VStack,
-  useDisclosure,
-  AlertDialog,
-  AlertDialogOverlay,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogBody,
-  Button,
-} from "@chakra-ui/react";
+import { HStack, VStack, useDisclosure } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { defineMessages, useIntl } from "react-intl";
 
+import { DeleteAccountModal } from "@/components/DeleteAccountModal/DeleteAccountModal";
 import { trpcReact } from "@/providers/TRPCProvider";
 import { TextInput } from "@/ui/Forms/TextInput/TextInput";
 import { PillButton } from "@/ui/PillButton/PillButton";
@@ -36,19 +26,6 @@ const messages = defineMessages({
   deleteAccount: {
     defaultMessage: "Delete Account",
   },
-  deleteAccountTitle: {
-    defaultMessage: "Delete Account",
-  },
-  deleteAccountConfirmation: {
-    defaultMessage:
-      "Are you sure? You'll have to re-import this account if you want to use it again.",
-  },
-  cancel: {
-    defaultMessage: "Cancel",
-  },
-  delete: {
-    defaultMessage: "Delete",
-  },
 });
 
 export function AccountSettings({ accountName }: Props) {
@@ -57,6 +34,24 @@ export function AccountSettings({ accountName }: Props) {
   const { formatMessage } = useIntl();
 
   const [newName, setNewName] = useState(accountName);
+
+  const { data: isSyncedData } = trpcReact.isAccountSynced.useQuery(
+    {
+      account: accountName,
+    },
+    {
+      refetchInterval: 5000,
+    },
+  );
+
+  const { data: accountData } = trpcReact.getAccount.useQuery(
+    {
+      name: accountName,
+    },
+    {
+      refetchInterval: 5000,
+    },
+  );
 
   // @todo: Renaming the account causes the parent component to refetch an account which no longer exists,
   // causing an error. The error doesn't cause any issues, because once the redirect happens, the parent
@@ -72,13 +67,6 @@ export function AccountSettings({ accountName }: Props) {
       },
     });
 
-  const { mutate: deleteAccount, isLoading: isDeleteLoading } =
-    trpcReact.deleteAccount.useMutation({
-      onSuccess: () => {
-        router.replace("/accounts");
-      },
-    });
-
   const hasValidName = newName.length > 0;
 
   const {
@@ -86,7 +74,6 @@ export function AccountSettings({ accountName }: Props) {
     onOpen: onDeleteModalOpen,
     onClose: onDeleteModalClose,
   } = useDisclosure();
-  const cancelDeleteRef = useRef(null);
 
   return (
     <>
@@ -125,39 +112,14 @@ export function AccountSettings({ accountName }: Props) {
           </PillButton>
         </HStack>
       </VStack>
-      <AlertDialog
-        isOpen={isDeleteModalOpen}
-        leastDestructiveRef={cancelDeleteRef}
-        onClose={onDeleteModalClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              {formatMessage(messages.deleteAccountTitle)}
-            </AlertDialogHeader>
-
-            <AlertDialogBody>
-              {formatMessage(messages.deleteAccountConfirmation)}
-            </AlertDialogBody>
-
-            <AlertDialogFooter>
-              <Button ref={cancelDeleteRef} onClick={onDeleteModalClose}>
-                {formatMessage(messages.cancel)}
-              </Button>
-              <Button
-                isDisabled={isDeleteLoading}
-                colorScheme="red"
-                onClick={() => {
-                  deleteAccount({ account: accountName });
-                }}
-                ml={3}
-              >
-                {formatMessage(messages.delete)}
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+      {isSyncedData && accountData && (
+        <DeleteAccountModal
+          isOpen={isDeleteModalOpen}
+          onClose={onDeleteModalClose}
+          isSynced={isSyncedData.synced}
+          account={accountData}
+        />
+      )}
     </>
   );
 }
---
+++ renderer/components/DeleteAccountModal/DeleteAccountModal.tsx
@@ -0,0 +1,253 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalFooter,
+  ModalBody,
+  Heading,
+  Text,
+  VStack,
+  Box,
+  Progress,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useCallback, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { defineMessages, useIntl } from "react-intl";
+
+import { getTrpcVanillaClient, trpcReact } from "@/providers/TRPCProvider";
+import { TextInput } from "@/ui/Forms/TextInput/TextInput";
+import { PillButton } from "@/ui/PillButton/PillButton";
+import { formatOre } from "@/utils/ironUtils";
+
+const messages = defineMessages({
+  deleteAccount: {
+    defaultMessage: "Delete Account",
+  },
+  notSyncedWarning: {
+    defaultMessage:
+      "This account is not yet synced and may contain $IRON or other assets.",
+  },
+  hasBalanceWarning: {
+    defaultMessage: "This account currently holds the following assets:",
+  },
+  loseAssetsWarning: {
+    defaultMessage:
+      "If you delete this account without exporting it first, YOU WILL LOSE THESE ASSETS.",
+  },
+  loading: {
+    defaultMessage: "Loading...",
+  },
+  tryAgain: {
+    defaultMessage: "Try Again",
+  },
+  somethingWentWrong: {
+    defaultMessage: "Something went wrong, please try again.",
+  },
+  cancel: {
+    defaultMessage: "Cancel",
+  },
+  deleteAccountButton: {
+    defaultMessage: "Delete Account",
+  },
+  closeButton: {
+    defaultMessage: "Close",
+  },
+  areYouSure: {
+    defaultMessage: "Are you sure you want to delete the account?",
+  },
+  typeToDelete: {
+    defaultMessage: "Type ''{accountName}'' to delete",
+  },
+  textMustMatch: {
+    defaultMessage: "Text must match the account name ''{accountName}''",
+  },
+});
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  isSynced: boolean;
+  account: Awaited<
+    ReturnType<ReturnType<typeof getTrpcVanillaClient>["getAccount"]["query"]>
+  >;
+};
+
+export function DeleteAccountModal({
+  isOpen,
+  onClose,
+  account,
+  isSynced,
+}: Props) {
+  const router = useRouter();
+
+  const {
+    mutate: deleteAccount,
+    isIdle,
+    isLoading,
+    isError,
+    isSuccess,
+    reset,
+  } = trpcReact.deleteAccount.useMutation({
+    onSuccess: () => {
+      router.replace("/accounts");
+    },
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset: resetForm,
+  } = useForm<{ name: string }>();
+
+  const { formatMessage } = useIntl();
+
+  const handleClose = useCallback(() => {
+    reset();
+    resetForm();
+    onClose();
+  }, [onClose, reset, resetForm]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      reset();
+      resetForm();
+      handleClose();
+    }
+  }, [handleClose, isSuccess, reset, resetForm]);
+
+  const balances = [account.balances.iron, ...account.balances.custom].filter(
+    (b) => b.unconfirmed !== "0",
+  );
+  const hasBalance = balances.length > 0;
+
+  return (
+    <Modal isOpen={isOpen} onClose={handleClose}>
+      <ModalOverlay />
+      <ModalContent maxW="100%" width="600px">
+        <ModalBody px={16} pt={16}>
+          {!isError && (
+            <>
+              <Heading fontSize="2xl" mb={4}>
+                {formatMessage(messages.deleteAccount)}
+              </Heading>
+
+              {!isSynced && (
+                <Text fontSize="md" mb={6}>
+                  {formatMessage(messages.notSyncedWarning)}
+                </Text>
+              )}
+
+              {isSynced && hasBalance && (
+                <>
+                  <Text fontSize="md" mb={6}>
+                    {formatMessage(messages.hasBalanceWarning)}
+                  </Text>
+                  <UnorderedList mb={6}>
+                    {balances.map((b) => (
+                      <ListItem key={b.assetId}>{`${formatOre(b.unconfirmed)} ${
+                        b.asset.name
+                      }`}</ListItem>
+                    ))}
+                  </UnorderedList>
+                </>
+              )}
+
+              {(!isSynced || hasBalance) && (
+                <Text fontSize="md" mb={6}>
+                  {formatMessage(messages.loseAssetsWarning)}
+                </Text>
+              )}
+
+              <Text fontSize="md" mb={6}>
+                {formatMessage(messages.areYouSure, {
+                  accountName: account.name,
+                })}
+              </Text>
+
+              {isError && (
+                <Text fontSize="md">
+                  {formatMessage(messages.somethingWentWrong)}
+                </Text>
+              )}
+
+              {isLoading ? (
+                <Box py={8}>
+                  <Progress size="sm" isIndeterminate />
+                </Box>
+              ) : (
+                <VStack gap={4} alignItems="stretch">
+                  <TextInput
+                    {...register("name", { pattern: new RegExp(account.name) })}
+                    label={formatMessage(messages.typeToDelete, {
+                      accountName: account.name,
+                    })}
+                    error={
+                      errors.name &&
+                      formatMessage(messages.textMustMatch, {
+                        accountName: account.name,
+                      })
+                    }
+                  />
+                </VStack>
+              )}
+            </>
+          )}
+        </ModalBody>
+
+        <ModalFooter display="flex" gap={2} px={16} py={8}>
+          {(isIdle || isLoading) && (
+            <>
+              <PillButton
+                size="sm"
+                isDisabled={isLoading}
+                onClick={handleClose}
+                variant="inverted"
+                border={0}
+              >
+                {formatMessage(messages.cancel)}
+              </PillButton>
+              <PillButton
+                size="sm"
+                isDisabled={isLoading}
+                onClick={() => {
+                  handleSubmit(() => {
+                    deleteAccount({ account: account.name });
+                  })();
+                }}
+              >
+                {formatMessage(messages.deleteAccountButton)}
+              </PillButton>
+            </>
+          )}
+          {isError && (
+            <>
+              <PillButton
+                size="sm"
+                isDisabled={isLoading}
+                onClick={onClose}
+                variant="inverted"
+                border={0}
+              >
+                {formatMessage(messages.closeButton)}
+              </PillButton>
+              <PillButton
+                size="sm"
+                isDisabled={isLoading}
+                onClick={() => {
+                  resetForm();
+                  reset();
+                }}
+              >
+                {formatMessage(messages.tryAgain)}
+              </PillButton>
+            </>
+          )}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
