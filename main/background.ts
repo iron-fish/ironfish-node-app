9cfe4608a961acb80a41f26683daf9f71ce933de
Derek Guenther <dguenther9@gmail.com>
2024-01-17 19:12:08 -0500

    Use transparent background for windows WindowControl buttons

--- main/background.ts
+++ main/background.ts
@@ -55,12 +55,12 @@ async function createThemeChangeHandler() {
       if (nativeTheme.shouldUseDarkColors) {
         // setTitleBarOverlay is undefined on non-Windows platforms
         mw.setTitleBarOverlay?.({
-          color: "#101010",
+          color: "#10101000",
           symbolColor: "#ffffff",
         });
       } else {
         mw.setTitleBarOverlay?.({
-          color: "#ffffff",
+          color: "#ffffff00",
           symbolColor: "#101010",
         });
       }
