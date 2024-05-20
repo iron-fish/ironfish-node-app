2bac2b3f65ddbdf9135c92b2ad89f01436005b8a
Daniel Cortes <3639170+dgca@users.noreply.github.com>
2024-05-20 16:56:54 -0600

    Bridge loading UI (#200)
    * Loading UI
    
    * Fix type import

--- renderer/components/BridgeAssetsForm/BridgeAssetsForm.tsx
+++ renderer/components/BridgeAssetsForm/BridgeAssetsForm.tsx
@@ -1,4 +1,4 @@
-import { Box, chakra, Flex, HStack, Text, VStack } from "@chakra-ui/react";
+import { HStack, Skeleton, Text } from "@chakra-ui/react";
 import { useMemo } from "react";
 import { useForm } from "react-hook-form";
 import { defineMessages, useIntl } from "react-intl";
@@ -7,19 +7,21 @@ import { TRPCRouterOutputs, trpcReact } from "@/providers/TRPCProvider";
 import { COLORS } from "@/ui/colors";
 import { Select } from "@/ui/Forms/Select/Select";
 import { TextInput } from "@/ui/Forms/TextInput/TextInput";
-import { PillButton } from "@/ui/PillButton/PillButton";
-import { BridgeArrows } from "@/ui/SVGs/BridgeArrows";
 import {
   ChainportToken,
   useChainportTokens,
 } from "@/utils/chainport/chainport";
 
+import { BridgeAssetsFormShell } from "./BridgeAssetsFormShell";
 import { AssetAmountInput } from "../AssetAmountInput/AssetAmountInput";
 import { useAccountAssets } from "../AssetAmountInput/utils";
 
 const messages = defineMessages({
   fromLabel: {
     defaultMessage: "From Account",
+  },
+  needHelp: {
+    defaultMessage: "Need help?",
   },
 });
 
@@ -91,87 +93,42 @@ export function BridgeAssetsFormContent({
   }
 
   return (
-    <chakra.form
-      onSubmit={(e) => {
-        e.preventDefault();
-        console.log("Handle submit...");
-      }}
-    >
-      <VStack gap={4} alignItems="stretch">
+    <BridgeAssetsFormShell
+      fromAccountInput={
         <Select
           {...register("fromAccount")}
           value={fromAccountValue}
           label={formatMessage(messages.fromLabel)}
           options={accountOptions}
         />
-        <VStack alignItems="stretch" gap={0}>
-          <VStack
-            p={8}
-            borderRadius={4}
-            bg={COLORS.GRAY_LIGHT}
-            alignItems="stretch"
-            gap={4}
-            _dark={{
-              bg: "transparent",
-              border: `1px solid ${COLORS.DARK_MODE.GRAY_MEDIUM}`,
-            }}
-          >
-            <AssetAmountInput
-              assetOptions={bridgeableAssets}
-              assetOptionsMap={assetOptionsMap}
-              amountValue={amountValue}
-              onAmountChange={(value) => console.log(value)}
-              assetIdValue={assetIdValue}
-              onAssetIdChange={async (value) => console.log(value)}
-            />
-            <HStack gap={4}>
-              <TextInput isReadOnly label="Bridge Provider" value="Chainport" />
-              <Text color={COLORS.GRAY_MEDIUM}>Need help?</Text>
-            </HStack>
-          </VStack>
-          <Box my="2.5px" position="relative">
-            <Flex
-              bg="#F3DEF5"
-              color={COLORS.ORCHID}
-              border="3px solid white"
-              borderRadius={4}
-              alignItems="center"
-              justifyContent="center"
-              position="absolute"
-              h="39px"
-              w="42px"
-              left="50%"
-              top="50%"
-              transform="translateX(-50%) translateY(-50%)"
-            >
-              <BridgeArrows />
-            </Flex>
-          </Box>
-          <VStack
-            p={8}
-            borderRadius={4}
-            bg={COLORS.GRAY_LIGHT}
-            alignItems="stretch"
-            _dark={{
-              bg: "transparent",
-              border: `1px solid ${COLORS.DARK_MODE.GRAY_MEDIUM}`,
-            }}
-          >
-            <HStack>
-              <Select
-                {...register("destinationNetwork")}
-                value={destinationNetworkValue}
-                label="Destination network"
-                options={availableNetworks}
-              />
-            </HStack>
-          </VStack>
-        </VStack>
-      </VStack>
-      <HStack mt={8} justifyContent="flex-end">
-        <PillButton type="submit">Bridge Asset</PillButton>
-      </HStack>
-    </chakra.form>
+      }
+      assetAmountInput={
+        <AssetAmountInput
+          assetOptions={bridgeableAssets}
+          assetOptionsMap={assetOptionsMap}
+          amountValue={amountValue}
+          onAmountChange={(value) => console.log(value)}
+          assetIdValue={assetIdValue}
+          onAssetIdChange={async (value) => console.log(value)}
+        />
+      }
+      bridgeProviderInput={
+        <HStack gap={4}>
+          <TextInput isReadOnly label="Bridge Provider" value="Chainport" />
+          <Text color={COLORS.GRAY_MEDIUM}>
+            {formatMessage(messages.needHelp)}
+          </Text>
+        </HStack>
+      }
+      destinationNetworkInput={
+        <Select
+          {...register("destinationNetwork")}
+          value={destinationNetworkValue}
+          label="Destination network"
+          options={availableNetworks}
+        />
+      }
+    />
   );
 }
 
@@ -182,7 +139,15 @@ export function BridgeAssetsForm() {
     useChainportTokens();
 
   if (!filteredAccounts || isChainportLoading) {
-    return <Text>Loading! Make this look good!</Text>;
+    return (
+      <BridgeAssetsFormShell
+        status="LOADING"
+        fromAccountInput={<Skeleton height={71} />}
+        assetAmountInput={<Skeleton height={71} />}
+        bridgeProviderInput={<Skeleton height={71} w="50%" />}
+        destinationNetworkInput={<Skeleton height={71} w="50%" />}
+      />
+    );
   }
 
   if (!tokensData) {
