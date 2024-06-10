9a8a13047ef9dd7e09d85c8421e7bf5fe798a84d
Daniel Cortes <danielgcortes@gmail.com>
2024-06-10 00:12:26 -0600

    Add Chainport route handlers

---
+++ main/api/chainport/index.ts
@@ -0,0 +1,167 @@
+import axios from "axios";
+import { z } from "zod";
+
+import { handleGetChainportBridgeTransactionEstimatedFees } from "./handleGetChainportBridgeTransactionEstimatedFees";
+import { handleGetChainportTransactionStatus } from "./handleGetChainportTransactionStatus";
+import {
+  handleSendChainportBridgeTransaction,
+  handleSendChainportBridgeTransactionInput,
+} from "./handleSendChainportBridgeTransaction";
+import { buildTransactionRequestParamsInputs } from "./utils/buildTransactionRequestParams";
+import { decodeChainportMemo } from "./utils/decodeChainportMemo";
+import { getChainportEndpoints } from "./utils/getChainportEndpoints";
+import {
+  ChainportBridgeTransaction,
+  ChainportTargetNetwork,
+  ChainportToken,
+  assertMetadataApiResponse,
+  assertTokensApiResponse,
+} from "../../../shared/chainport";
+import { t } from "../trpc";
+
+export const chainportRouter = t.router({
+  getChainportEndpoints: t.procedure.query(async () => {
+    const endpoints = await getChainportEndpoints();
+    return endpoints;
+  }),
+  getChainportTokens: t.procedure.query(async () => {
+    const { tokensEndpoint, metadataEndpoint } = await getChainportEndpoints();
+
+    try {
+      const [tokensResponse, metadataResponse] = await Promise.all([
+        axios.get(tokensEndpoint),
+        axios.get(metadataEndpoint),
+      ]);
+      const tokensData = assertTokensApiResponse(tokensResponse.data);
+      const chainportMeta = assertMetadataApiResponse(metadataResponse.data);
+
+      const chainportTokens = tokensData.verified_tokens.map((token) => {
+        const targetNetworks: Array<ChainportTargetNetwork> =
+          token.target_networks
+            .map((networkId) => {
+              const networkDetails = chainportMeta.cp_network_ids[networkId];
+              if (!networkDetails) {
+                throw new Error(`Unknown network id: ${networkId}`);
+              }
+              return {
+                chainportNetworkId: networkId,
+                label: networkDetails.label,
+                networkIcon: networkDetails.network_icon,
+                chainId: networkDetails.chain_id,
+                value: networkDetails.chain_id
+                  ? networkDetails.chain_id.toString()
+                  : "",
+              };
+            })
+            .filter((item) => {
+              return item.value !== null;
+            });
+
+        return {
+          chainportId: token.id,
+          ironfishId: token.web3_address,
+          symbol: token.symbol,
+          name: token.name,
+          decimals: token.decimals,
+          targetNetworks,
+        };
+      });
+
+      const tokenEntries = chainportTokens.map<[string, ChainportToken]>(
+        (token) => [token.ironfishId, token],
+      );
+      const chainportTokensMap: Record<string, ChainportToken> =
+        Object.fromEntries(tokenEntries);
+
+      const networksEntries = chainportTokens.flatMap((token) =>
+        token.targetNetworks.map<[string, ChainportTargetNetwork]>(
+          (network) => [network.value, network],
+        ),
+      );
+      const chainportNetworksMap: Record<string, ChainportTargetNetwork> =
+        Object.fromEntries(networksEntries);
+      return {
+        chainportTokens,
+        chainportTokensMap,
+        chainportNetworksMap,
+      };
+    } catch (err) {
+      console.error("Failed to fetch chainport data", err);
+      throw err;
+    }
+  }),
+  getChainportBridgeTransactionDetails: t.procedure
+    .input(
+      z.object({
+        amount: z.string(),
+        assetId: z.string(),
+        to: z.string(),
+        selectedNetwork: z.string(),
+      }),
+    )
+    .query(async (opts) => {
+      const endpoints = await getChainportEndpoints();
+
+      const { amount, assetId, to, selectedNetwork } = opts.input;
+
+      const url = `${
+        endpoints.baseUrl
+      }/ironfish/metadata?raw_amount=${amount.toString()}&asset_id=${assetId}&target_network_id=${selectedNetwork}&target_web3_address=${to}`;
+
+      const response: {
+        data: ChainportBridgeTransaction;
+      } = await axios.get(url);
+
+      return response.data;
+    }),
+  getChainportBridgeTransactionEstimatedFees: t.procedure
+    .input(buildTransactionRequestParamsInputs)
+    .query(async (opts) => {
+      const result = await handleGetChainportBridgeTransactionEstimatedFees(
+        opts.input,
+      );
+      return result;
+    }),
+  sendChainportBridgeTransaction: t.procedure
+    .input(handleSendChainportBridgeTransactionInput)
+    .mutation(async (opts) => {
+      const result = await handleSendChainportBridgeTransaction(opts.input);
+      return result;
+    }),
+  getChainportTransactionStatus: t.procedure
+    .input(
+      z.object({
+        transactionHash: z.string(),
+        baseNetworkId: z.number(),
+      }),
+    )
+    .query(async (opts) => {
+      const result = handleGetChainportTransactionStatus({
+        txHash: opts.input.transactionHash,
+        baseNetworkId: opts.input.baseNetworkId,
+      });
+      return result;
+    }),
+  getChainportMeta: t.procedure.query(async () => {
+    const { metadataEndpoint } = await getChainportEndpoints();
+    const response = await axios.get(metadataEndpoint);
+    const data = assertMetadataApiResponse(response.data);
+    return data;
+  }),
+  decodeMemo: t.procedure
+    .input(
+      z.object({
+        memo: z.string(),
+      }),
+    )
+    .query(async (opts) => {
+      try {
+        const result = decodeChainportMemo(
+          Buffer.from(opts.input.memo).toString(),
+        );
+        return result;
+      } catch (_err) {
+        return null;
+      }
+    }),
+});
---
+++ shared/chainport.ts
@@ -0,0 +1,139 @@
+import { z } from "zod";
+
+export type ChainportToken = {
+  chainportId: number;
+  ironfishId: string;
+  symbol: string;
+  name: string;
+  decimals: number;
+  targetNetworks: {
+    chainportNetworkId: number;
+    chainId: number | null;
+    value: string;
+    label: string;
+    networkIcon: string;
+  }[];
+};
+
+export type ChainportTargetNetwork = {
+  chainportNetworkId: number;
+  label: string;
+  networkIcon: string;
+  chainId: number | null;
+  value: string;
+};
+
+const MetadataApiResponseSchema = z.object({
+  maintenance: z.boolean(),
+  sorted_networks: z.array(z.string()),
+  cp_network_ids: z.record(
+    z.object({
+      minimum_confirmation: z.number().nullable(),
+      explorer_url: z.string(),
+      public_rpc: z.string().nullable(),
+      chainport_network_id: z.number(),
+      shortname: z.string(),
+      name: z.string(),
+      utils_contract: z.union([z.string(), z.null()]),
+      chain_id: z.number().nullable(),
+      label: z.string(),
+      blockchain_type: z.string(),
+      native_token_symbol: z.string(),
+      button_colour: z.string(),
+      network_icon: z.string(),
+      bridges: z.record(z.string()),
+    }),
+  ),
+  abis: z.object({
+    Sdk: z.string(),
+    ChainportMainBridge: z.string(),
+    ChainportSideBridge: z.string(),
+    LifiBridge: z.string(),
+    ChainportUtils: z.string(),
+    UsdcMessageTransmitter: z.string(),
+    BridgeMintableTokenV2: z.string(),
+    ChainportFeeManager: z.string(),
+    UsdcBridge: z.string(),
+    StablesWrapper: z.string(),
+    StablesSwap: z.string(),
+    ChainportStableBridge: z.string(),
+    StablesStaking: z.string(),
+  }),
+  tvl: z.string(),
+  native_coins_usd_values: z.record(z.number()),
+  fee_manager: z.string(),
+  perpetual_contract: z.string(),
+  portx_addresses: z.record(z.string()),
+  stables_staking_contracts: z.record(z.string()),
+  stable_coins_usd_values: z.record(z.number()),
+  usdc_destination_domains: z.record(z.number()),
+  usdc_token_addresses_by_network: z.record(z.string()),
+  port_time_estimates_secs: z.record(z.number()),
+});
+
+export type MetaApiResponse = z.infer<typeof MetadataApiResponseSchema>;
+
+export function assertMetadataApiResponse(data: unknown): MetaApiResponse {
+  return MetadataApiResponseSchema.parse(data);
+}
+
+const TokensApiResponseSchema = z.object({
+  verified_tokens: z.array(
+    z.object({
+      decimals: z.number(),
+      id: z.number(),
+      name: z.string(),
+      pinned: z.boolean(),
+      web3_address: z.string(),
+      symbol: z.string(),
+      token_image: z.string(),
+      target_networks: z.array(z.number()),
+      chain_id: z.null(),
+      network_name: z.string(),
+      network_id: z.number(),
+      blockchain_type: z.string(),
+      is_stable: z.boolean(),
+      is_lifi: z.boolean(),
+    }),
+  ),
+  blacklisted_addresses: z.array(z.string()),
+  tokens_exact_amount: z.array(z.unknown()).optional(),
+});
+
+export type TokensApiResponse = z.infer<typeof TokensApiResponseSchema>;
+
+export function assertTokensApiResponse(data: unknown): TokensApiResponse {
+  return TokensApiResponseSchema.parse(data);
+}
+
+export type ChainportBridgeTransaction = {
+  bridge_output: {
+    publicAddress: string;
+    amount: string;
+    memoHex: string;
+    assetId: string;
+  };
+  gas_fee_output: {
+    publicAddress: string;
+    amount: string;
+    memo: string;
+  };
+  bridge_fee: {
+    source_token_fee_amount: number | null;
+    portx_fee_amount: number;
+    is_portx_fee_payment: boolean;
+  };
+};
+
+export type ChainportTransactionStatus = {
+  base_network_id?: number;
+  base_tx_hash?: string;
+  base_tx_status?: number;
+  base_token_address?: string;
+  target_network_id?: number;
+  target_tx_hash?: string;
+  target_tx_status?: number;
+  target_token_address?: string;
+  created_at?: string;
+  port_in_ack?: boolean;
+};
