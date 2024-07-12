9e7d874ee2c644b1b24b38caa246a52a83d78ba0
Daniel Cortes <danielgcortes@gmail.com>
2024-07-12 00:16:16 -0600

    Clean up

--- module-patches/patches/usb.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-
-var usb = require("node-gyp-build")("node_modules/usb");
-module.exports = usb;
