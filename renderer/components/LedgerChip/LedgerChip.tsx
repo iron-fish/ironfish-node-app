9334e26ab145b2f6c6eb03e4c3933fee79e7e860
Daniel Cortes <danielgcortes@gmail.com>
2024-07-24 17:03:20 -0600

    Refactor chip into its own component

--- renderer/components/LedgerChip/LedgerChip.tsx
+++ renderer/components/LedgerChip/LedgerChip.tsx
@@ -1,30 +1,5 @@
-import { Text, HStack } from "@chakra-ui/react";
-
-import { COLORS } from "@/ui/colors";
+import { InfoChip } from "@/ui/InfoChip/InfoChip";
 
 export function LedgerChip() {
-  return (
-    <HStack
-      alignItems="center"
-      bg={COLORS.GRAY_LIGHT}
-      color={COLORS.GRAY_MEDIUM}
-      spacing={1}
-      borderRadius="full"
-      border="1px solid transparent"
-      px="8px"
-      py="2px"
-      _hover={{
-        borderColor: COLORS.GRAY_MEDIUM,
-      }}
-      _dark={{
-        bg: COLORS.DARK_MODE.GRAY_DARK,
-        color: COLORS.DARK_MODE.GRAY_LIGHT,
-        _hover: {
-          borderColor: COLORS.DARK_MODE.GRAY_LIGHT,
-        },
-      }}
-    >
-      <Text fontSize="12px">Ledger</Text>
-    </HStack>
-  );
+  return <InfoChip label="Ledger" />;
 }
