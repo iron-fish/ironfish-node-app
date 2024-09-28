8516db0886b5bb09b39c7d0e529548c9cefd17cc
Rahul Patni <rahul@iflabs.network>
2024-09-27 17:28:20 -0700

    using old chainport endpoint as a temp fix before launch

--- main/api/chainport/utils/getChainportEndpoints.ts
+++ main/api/chainport/utils/getChainportEndpoints.ts
@@ -1,13 +1,18 @@
+import { MAINNET } from "@ironfish/sdk";
+
 import { manager } from "../../manager";
 
 export async function getChainportEndpoints() {
   const ironfish = await manager.getIronfish();
   const rpcClient = await ironfish.rpcClient();
   const response = await rpcClient.chain.getNetworkInfo();
+
+  const networkId = response.content.networkId.toString();
+
   const prefix = {
     "0": "preprod-",
     "1": "",
-  }[response.content.networkId.toString()];
+  }[networkId];
 
   if (typeof prefix !== "string") {
     throw new Error(
@@ -16,7 +21,11 @@ export async function getChainportEndpoints() {
   }
 
   const baseUrl = `https://${prefix}api.chainport.io`;
-  const tokensEndpoint = `${baseUrl}/token_list?network_name=IRONFISH`;
+  const tokensEndpoint =
+    networkId === MAINNET.id.toString()
+      ? `${baseUrl}/token/list?network_name=IRONFISH`
+      : `${baseUrl}/token_list?network_name=IRONFISH`;
+  console.log("tokensEndpoint", tokensEndpoint);
   const metadataEndpoint = `${baseUrl}/meta`;
 
   return { baseUrl, tokensEndpoint, metadataEndpoint };
