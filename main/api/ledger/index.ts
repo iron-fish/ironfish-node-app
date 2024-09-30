4385ea888d74ad728e9de5a76eac5c18bf646592
Joe <joe@iflabs.network>
2024-09-30 14:27:26 -0700

    add exports to ledger router for dkg signing

--- main/api/ledger/index.ts
+++ main/api/ledger/index.ts
@@ -1,6 +1,9 @@
 import { observable } from "@trpc/server/observable";
 import { z } from "zod";
 
+import { handleAggregateSignatureShares } from "./multisig/handleAggregateSignatureShares";
+import { handleCreateSigningPackage } from "./multisig/handleCreateSigningPackage";
+import { ledgerDkg } from "./utils/dkg";
 import { ledgerManager, ConnectionStatus } from "./utils/ledger";
 import { handleSendTransactionInput } from "../transactions/handleSendTransaction";
 import { t } from "../trpc";
@@ -37,5 +40,48 @@ export const ledgerRouter = t.router({
     )
     .mutation((opts) => {
       return ledgerManager.markAccountAsLedger(opts.input.publicAddress);
+    }),
+  createSigningCommitment: t.procedure
+    .input(z.object({ txHash: z.string() }))
+    .mutation(async (opts) => {
+      return ledgerDkg.dkgGetCommitments(opts.input.txHash);
+    }),
+  createSignatureShare: t.procedure
+    .input(
+      z.object({
+        randomness: z.string(),
+        signingPackage: z.string(),
+        txHash: z.string(),
+      }),
+    )
+    .mutation(async (opts) => {
+      return ledgerDkg.dkgSign(
+        opts.input.randomness,
+        opts.input.signingPackage,
+        opts.input.txHash,
+      );
+    }),
+  aggregateSignatureShares: t.procedure
+    .input(
+      z.object({
+        account: z.string().optional(),
+        signingPackage: z.string(),
+        signatureShares: z.array(z.string()),
+        broadcast: z.boolean().optional(),
+      }),
+    )
+    .mutation(async (opts) => {
+      return handleAggregateSignatureShares({ request: opts.input });
+    }),
+  createSigningPackage: t.procedure
+    .input(
+      z.object({
+        account: z.string().optional(),
+        unsignedTransaction: z.string(),
+        commitments: z.array(z.string()),
+      }),
+    )
+    .mutation(async (opts) => {
+      return handleCreateSigningPackage({ request: opts.input });
     }),
 });
