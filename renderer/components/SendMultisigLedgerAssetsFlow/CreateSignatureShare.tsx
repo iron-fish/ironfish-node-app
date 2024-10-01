1d83703c1a50068f701a455c08769bfbcd72530b
Rahul Patni <rahul@iflabs.network>
2024-10-01 15:26:14 -0700

    trying

--- renderer/components/SendMultisigLedgerAssetsFlow/CreateSignatureShare.tsx
+++ renderer/components/SendMultisigLedgerAssetsFlow/CreateSignatureShare.tsx
@@ -5,77 +5,69 @@ import { trpcReact } from "@/providers/TRPCProvider";
 import { TextareaInput } from "@/ui/Forms/TextareaInput/TextareaInput";
 import { PillButton } from "@/ui/PillButton/PillButton";
 
-import { CopyField } from "./CopyField";
+export function CreateSignatureShare({
+  signingPackage,
+  unsignedTransaction,
+  onSubmit,
+}: {
+  signingPackage: string;
+  unsignedTransaction: string;
+  onSubmit: (signatureShares: string[]) => void;
+}) {
+  const {
+    mutate: createSignatureShare,
+    data: signatureShare,
+    isIdle,
+    isLoading,
+    isError,
+    isSuccess,
+    error,
+    reset,
+  } = trpcReact.createSignatureShare.useMutation();
 
-type SigningRole = "participant" | "coordinator"
+  const [signatureShares, setSignatureShares] = useState("");
 
-export function CreateSignatureShare({ signingPackage, unsignedTransaction, onSubmit, role }: { role: SigningRole, signingPackage: string, unsignedTransaction: string, onSubmit: (signatureShares: string[]) => void } ) {
-    const {
-        mutate: createSignatureShare,
-        data: signatureShare,
-        isIdle,
-        isLoading,
-        isError,
-        isSuccess,
-        error,
-        reset,
-    } = trpcReact.createSignatureShare.useMutation();
-
-    const [signatureShares, setSignatureShares] = useState("")
-
-    if (isIdle) {
-      createSignatureShare({ signingPackage, unsignedTransaction });
-    } else if (isSuccess) {
-      if (role === "coordinator") {
-        return <Box>
-          <TextareaInput
-              label="Enter Signature Shares from Participant Separated by Commas"
-              value={signatureShares}
-              onChange={(e) => { setSignatureShares(e.target.value) }}
-          />
-          <PillButton
-              mt={8}
-              height="60px"
-              px={8}
-              onClick={() => {
-                const shares = signatureShares.split(",");
-                shares.push(signatureShare);
-                onSubmit(shares);
-              }}
-          >
-              Next
-          </PillButton>
-        </Box>
-      }
-
-      return <>
-        <h1>Send the signature share to the coordinator</h1>
-        <CopyField label={"Signature Share"} value={signatureShare} />
+  if (isIdle) {
+    createSignatureShare({ signingPackage, unsignedTransaction });
+  } else if (isSuccess) {
+    return (
+      <Box>
+        <TextareaInput
+          label="Enter Signature Shares from Participant Separated by Commas"
+          value={signatureShares}
+          onChange={(e) => {
+            setSignatureShares(e.target.value);
+          }}
+        />
         <PillButton
-            mt={8}
-            height="60px"
-            px={8}
-            onClick={() => onSubmit([])}
-        >
-            Next
-        </PillButton>
-      </>
-  } else if (isError) {
-    return <div>
-      <h1>Error</h1>
-      <p>{error.message}</p>
-      <PillButton
           mt={8}
           height="60px"
           px={8}
-          onClick={reset}
-      >
+          onClick={() => {
+            const shares = signatureShares.split(",");
+            shares.push(signatureShare);
+            onSubmit(shares);
+          }}
+        >
+          Next
+        </PillButton>
+      </Box>
+    );
+  } else if (isError) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <PillButton mt={8} height="60px" px={8} onClick={reset}>
           Retry
-      </PillButton>
-    </div>
+        </PillButton>
+      </div>
+    );
   } else if (isLoading) {
-    return <div>
-      <h1>Creating Signature Share...</h1>
-    </div>
+    return (
+      <div>
+        <h1>Creating Signature Share...</h1>
+      </div>
+    );
   }
 }
--- renderer/components/SendMultisigLedgerAssetsFlow/CreateSigningCommitment.tsx
+++ renderer/components/SendMultisigLedgerAssetsFlow/CreateSigningCommitment.tsx
@@ -7,78 +7,72 @@ import { PillButton } from "@/ui/PillButton/PillButton";
 
 import { CopyField } from "./CopyField";
 
-type SigningRole = "participant" | "coordinator"
+export function CreateSigningCommitment({
+  txHash,
+  onSubmit,
+}: {
+  txHash: string;
+  onSubmit: (commitments: string[]) => void;
+}) {
+  const {
+    mutate: createSigningCommitment,
+    data: signingCommitment,
+    isIdle,
+    isLoading,
+    isError,
+    isSuccess,
+    error,
+    reset,
+  } = trpcReact.createSigningCommitment.useMutation();
 
-export function CreateSigningCommitment({ txHash, onSubmit, role }: { role: SigningRole, txHash: string, onSubmit: (commitments: string[]) => void } ) {
-    const {
-        mutate: createSigningCommitment,
-        data: signingCommitment,
-        isIdle,
-        isLoading,
-        isError,
-        isSuccess,
-        error,
-        reset,
-    } = trpcReact.createSigningCommitment.useMutation();
+  const [signingCommitments, setSigningCommitments] = useState("");
 
-    const [signingCommitments, setSigningCommitments] = useState("")
+  if (isIdle) {
+    createSigningCommitment({ txHash });
+  } else if (isSuccess) {
+    return (
+      <div>
+        <h1>Send the signing commitment to the coordinator</h1>
+        <CopyField label={"Signing Commitment"} value={signingCommitment} />
 
-    if (isIdle) {
-        createSigningCommitment({ txHash });
-    } else if (isSuccess) {
-        
-    return <div>
-      <h1>Send the signing commitment to the coordinator</h1>
-      <CopyField label={"Signing Commitment"} value={signingCommitment} />
-      {role === "coordinator" && (
         <Box>
           <TextareaInput
-              label="Enter Signing Commitments from Participant Separated by Commas"
-              value={signingCommitments}
-              onChange={(e) => { setSigningCommitments(e.target.value) }}
+            label="Enter Signing Commitments from Participant Separated by Commas"
+            value={signingCommitments}
+            onChange={(e) => {
+              setSigningCommitments(e.target.value);
+            }}
           />
           <PillButton
-              mt={8}
-              height="60px"
-              px={8}
-              onClick={() => {
-                const commitments = signingCommitments.split(",");
-                commitments.push(signingCommitment);
-                  onSubmit(commitments);
-              }}
-          >
-              Next
-          </PillButton>
-        </Box>
-      )}
-      {role === "participant" && (
-        <PillButton
             mt={8}
             height="60px"
             px={8}
-            onClick={() => onSubmit([])}
-        >
+            onClick={() => {
+              const commitments = signingCommitments.split(",");
+              commitments.push(signingCommitment);
+              onSubmit(commitments);
+            }}
+          >
             Next
-        </PillButton>
-      )}
-    </div>
+          </PillButton>
+        </Box>
+      </div>
+    );
   } else if (isError) {
-    return <div>
-      <h1>Error</h1>
-      <p>{error.message}</p>
-      <PillButton
-          mt={8}
-          height="60px"
-          px={8}
-          onClick={reset}
-      >
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <PillButton mt={8} height="60px" px={8} onClick={reset}>
           Retry
-      </PillButton>
-    </div>
+        </PillButton>
+      </div>
+    );
   } else if (isLoading) {
-    return <div>
-      <h1>Creating Signing Commitment...</h1>
-    </div>
+    return (
+      <div>
+        <h1>Creating Signing Commitment...</h1>
+      </div>
+    );
   }
-
 }
