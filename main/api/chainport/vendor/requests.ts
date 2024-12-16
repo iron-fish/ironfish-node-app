519f4f537e864cdcad51a49432a93f6b52f5101e
Derek Guenther <derek@iflabs.network>
2024-12-16 12:49:37 -0500

    Pass through API error messages when bridging (#297)

--- main/api/chainport/vendor/requests.ts
+++ main/api/chainport/vendor/requests.ts
@@ -85,11 +85,11 @@ const makeChainportRequest = async <T extends object>(
     })
     .catch((error) => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      const chainportError = error.response?.data?.error?.description as string;
-      if (chainportError) {
-        throw new Error(chainportError);
+      const apiError = error.response?.data?.message as string;
+      if (apiError) {
+        throw new Error("Chainport error: " + apiError);
       } else {
-        throw new Error("Chainport error - " + error);
+        throw new Error("Chainport error: " + error);
       }
     });
 
